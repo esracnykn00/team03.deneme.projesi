ac8f820f0bd33ec9625e5820a1299a164bb46b11
YaseminBilgic <kitapkurduyasemin44@gmail.com>
2023-08-03 16:04:10 +0300

    YaseminV2

--- src/main/java/deneme/deneme01.java
+++ src/main/java/deneme/deneme01.java
@@ -3,18 +3,22 @@ package deneme;
 public class deneme01 {
     public static void main(String[] args) {
         System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
+        System.out.println("deneme2");
+        System.out.println("deneme3");
+        System.out.println("deneme4");
+        System.out.println("deneme5");
+        System.out.println("deajkshj");
+        System.out.println("dejskjfs");
+        System.out.println("dedlkfflkfn1");
+        System.out.println("denemdfjnn");
+        System.out.println("denefdkk");
+        System.out.println("denemefdk");
+        System.out.println("deneme7");
+        System.out.println("deneme15");
+        System.out.println("denfısroıooncklcncdklsjo");
+
+        System.out.println("merhaba dünya");
+        System.out.println("Hello Word");
+
     }
 }
