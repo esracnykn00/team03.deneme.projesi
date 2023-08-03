11aa9845a4f030875700bfe456249f288b744421
Nuray Birinci <nuraybirinci6761@gmail.com>
2023-08-03 16:03:59 +0300

    nurayv2

--- src/main/java/deneme/deneme01.java
+++ src/main/java/deneme/deneme01.java
@@ -2,19 +2,21 @@ package deneme;
 
 public class deneme01 {
     public static void main(String[] args) {
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
+        System.out.println("denemeeurjcmcejkm");
+        System.out.println("deneme1ölklmjcncfn");
+        System.out.println("rckıejn");
+        System.out.println("fcrhnmkn");
+        System.out.println("d hbv");
+        System.out.println("vdfbcx");
+        System.out.println("knıuby");
+        System.out.println("ıolknjb");
+        System.out.println("rfrcsfc");
+        System.out.println("scy");
+        System.out.println("ukmıhu");
+        System.out.println("ddfbdss");
+        System.out.println("btkyt");
+        System.out.println("nömfhg");
+
+        System.out.println("kckkmvıcölemvkfkfm ");
     }
 }
