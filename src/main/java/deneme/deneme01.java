d3af08b28ee2f1189740f29a6b298d1fb490ccb5
esracnykn00 <esrakaraoglan00@gmail.com>
2023-08-03 16:03:55 +0300

    Esra V2

--- src/main/java/deneme/deneme01.java
+++ src/main/java/deneme/deneme01.java
@@ -2,19 +2,21 @@ package deneme;
 
 public class deneme01 {
     public static void main(String[] args) {
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
-        System.out.println("deneme1");
+        System.out.println("jhuhhfsdvhfjnvdflvjndfbkj");
+        System.out.println("gthyjyjuikykjuyk");
+        System.out.println("esdrftgyh");
+        System.out.println("gthyjkuilihhhhh");
+        System.out.println("nnmnmnmnmnm");
+        System.out.println("gggggggggggggg");
+        System.out.println("hhhhhhhhhhhhhhhh");
+        System.out.println("kkkkkkkkkkkkkkkkkk");
+        System.out.println("lllllllllllllllllllll");
+        System.out.println("mmmmmmmmmmmmmmmmmmmmmmmmm");
+        System.out.println("nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
+        System.out.println("ssssssssssssssssssssssssss");
+        System.out.println("cccccccccccccccccccccc");
+        System.out.println("bbbbbbbbbbbbbbbbb");
+
+        System.out.println("jjjjjjj");
     }
 }
